edb469375b05bdc356e3e045739b5bbb4752bb8e
John Downie <john@bluestone360.co.uk>
2020-03-06 21:49:46 +0000

    project employee many to many

--- src/main/java/com/jrp/pma/controllers/ProjectController.java
+++ src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entities.Employee;
 import com.jrp.pma.entities.Project;
@@ -20,16 +22,20 @@ public class ProjectController {
 	@Autowired
 	ProjectRepository proRepo;
 	
+	@Autowired
+	EmployeeRepository empRepo;
+	
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
 		
 		Project aProject = new Project();
+		List<Employee> employees = empRepo.findAll();
 		
 		model.addAttribute("project", aProject);
+		model.addAttribute("allEmployees", employees);
 		
-		List<Project> projects = proRepo.findAll();
-		
-		model.addAttribute("projectsList", projects);
+
+	
 		
 		return "projects/new-project";
 	}
@@ -38,7 +44,7 @@ public class ProjectController {
 	@PostMapping("/save")
 	public String createProject(Project project, Model model) {
 		proRepo.save(project);
-		
+				
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/projects";
 	}
--- src/main/java/com/jrp/pma/entities/Employee.java
+++ src/main/java/com/jrp/pma/entities/Employee.java
@@ -1,9 +1,16 @@
 package com.jrp.pma.entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Employee {
@@ -16,6 +23,15 @@ public class Employee {
 	private String lastName;
 	private String email;
 	
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST},
+			fetch = FetchType.LAZY)
+	@JoinTable(name="project_employee",
+		joinColumns=@JoinColumn(name="employee_id"),
+		inverseJoinColumns= @JoinColumn(name="project_id")
+	)
+	
+	private List<Project> projects;
+	
 	public Employee() {
 		
 	}
@@ -27,6 +43,14 @@ public class Employee {
 		this.email = email;
 	}
 	
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+
 	public long getEmployeeId() {
 		return employeeId;
 	}
