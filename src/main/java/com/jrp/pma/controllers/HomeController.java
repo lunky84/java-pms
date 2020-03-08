c37ae26e85b51f22c56b6e8d12e3e5c5e3f395dc
John Downie <john@bluestone360.co.uk>
2020-03-08 21:56:51 +0000

    added css and js

--- src/main/java/com/jrp/pma/controllers/HomeController.java
+++ src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Project;
 
 @Controller
@@ -16,11 +18,17 @@ public class HomeController {
 	@Autowired
 	ProjectRepository proRepo;
 	
+	@Autowired
+	EmployeeRepository empRepo;
+	
 	@GetMapping("/")
 	public String displayHome(Model model) {
-		List<Project> projects = proRepo.findAll();
 		
+		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectsList", projects);
+		
+		List<EmployeeProject> employeesProjectCnt = empRepo.employeeProjects();
+		model.addAttribute("employeesProjectCnt", employeesProjectCnt);
 		
 		return "main/home";
 	}
