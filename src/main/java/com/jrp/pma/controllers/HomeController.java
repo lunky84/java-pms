15c44f6b114856a1a8d9ce312266f590661f5a29
John Downie <john@bluestone360.co.uk>
2020-03-09 21:51:00 +0000

    passed data to js for pie chart

--- src/main/java/com/jrp/pma/controllers/HomeController.java
+++ src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -1,14 +1,19 @@
 package com.jrp.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.dto.ChartData;
 import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Project;
 
@@ -22,10 +27,20 @@ public class HomeController {
 	EmployeeRepository empRepo;
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
+		
+		Map<String, Object> map = new HashMap<>();
 		
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectsList", projects);
+		
+		List<ChartData> projectData = proRepo.getProjectStatus();
+		
+		// Lets convert projectData object into a json structure for use in Javascript
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		
+		model.addAttribute("projectStatusCnt", jsonString);
 		
 		List<EmployeeProject> employeesProjectCnt = empRepo.employeeProjects();
 		model.addAttribute("employeesProjectCnt", employeesProjectCnt);
